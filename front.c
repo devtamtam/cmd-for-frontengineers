54db4a3b150ac9b5b07a0200bac944a6683cedc8
devtamtam <115604773+devtamtam@users.noreply.github.com>
2023-06-03 23:25:33 +0900

    notify in case specifeid html cant be found

--- front.c
+++ front.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int fileExists(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file != NULL) {
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
-    if(argc!= 2) {printf("Usage:front htmlfilename\n"); }
-    else{
-        char command[100];
-        sprintf(command, "code %s", argv[1]);
-        system(command);
-        sprintf(command, "code style.css");
-        system(command);
-        sprintf(command, "start %s", argv[1]);
+    if (argc != 2) {
+        printf("Usage: front htmlfilename\n");
+    } else {
+        // Check if the HTML file exists
+        if (!fileExists(argv[1])) {
+            printf("HTML file not found: %s\ntry again\n", argv[1]);
+            return 1;
+        }
+
+        char command[200];
+        sprintf(command, "code %s && code style.css && start %s", argv[1], argv[1]);
         system(command);
     }
-
 
     return 0;
 }
